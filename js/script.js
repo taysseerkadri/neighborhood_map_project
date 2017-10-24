4b9cd185c1baf01f07ab639fb76b0e1817435441
Taysseer Kadri <taysseer.kadri@gmail.com>
2017-10-24 01:32:12 -0700

    Added Zomato API

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
 var map;
 var markers = [];
-var locationStore = [
-        {title: 'Birmingham Snow Hill', type: 'transit', location: {lat: 52.4767669, lng: -1.8962941}},
-        {title: 'Birmingham Moor Street', type: 'transit', location: {lat: 52.4776389, lng: -1.8960543}},
-        {title: 'Birmingham New Street Station', type: 'transit', location: {lat: 52.4776945, lng: -1.9012386}},
-
-        {title: 'Ming Moon Chinese Restaurant & Karaoke Bar', type: 'food', location: {lat: 52.4776711, lng: -1.8989897}},
-        {title: 'Jimmy Spices', type: 'food', location: {lat: 52.4763413, lng: -1.9073256}},
-        {title: 'Deep Caribbean Experience', type: 'food', location: {lat: 52.4730713, lng: -1.903475}},
-
-        {title: 'Radisson Blu Hotel, Birmingham', type: 'hotel', location: {lat: 52.4741191, lng: -1.8982193}},
-        {title: 'Ibis Birmingham New Street Station Hotel', type: 'hotel', location: {lat: 52.4741191, lng: -1.8982193}},
-        {title: 'Macdonald Burlington Hotel', type: 'hotel', location: {lat: 52.4766476, lng: -1.8989146}},
-
-        {title: 'Argos', type: 'shopping', location: {lat: 52.4766476, lng: -1.8989146}},
-        {title: 'John Lewis', type: 'shopping', location: {lat: 52.4766476, lng: -1.8989146}},
-        {title: 'Mailbox Birmingham', type: 'shopping', location: {lat: 52.4752309, lng: -1.9033766}},
-
-        {title: 'O2 Academy Birmingham', type: 'entertainment', location: {lat: 52.4752309, lng: -1.9033766}},
-        {title: 'The Glee Club', type: 'entertainment', location: {lat: 52.4752309, lng: -1.9033766}},
-        {title: 'Symphony Hall', type: 'entertainment', location: {lat: 52.4771366, lng: -1.9063213}},
-
-        {title: 'Genting Casino Chinatown Birmingham', type: 'nightlife', location: {lat: 52.4746765, lng: -1.9006145}},
-        {title: 'Boltz Club', type: 'nightlife', location: {lat: 52.4747277, lng: -1.9006566}},
-        {title: 'NUTS LIVEPOKER Limited', type: 'nightlife', location: {lat: 52.4908262, lng: -1.9108382}}
-    ];
+var zomatoCall = "https://developers.zomato.com/api/v2.1/search?apikey=774bb0d505fcc3058099aef1068ff9bd&lat=52.474942&lon=-1.8983275&radius=2000&count=20";
 var daystyles = [
     {
         featureType: 'administrative',
@@ -170,22 +146,36 @@ var nightstyles = [
         ]
     }
 ];
+var restaurants = {};
+
+$.ajax({
+    url: zomatoCall,
+    async: false,
+    dataType: 'json',
+    success: function(data){
+        restaurants = data['restaurants']
+    }
+});
 
 function initMap(style) {
-    if (style == null)
-    {
+    if (style == null) {
         style = daystyles
     }
-    map = new google.maps.Map(document.getElementById('map'),{
-        center: { lat: 52.4785217, lng: -1.8902777 },
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 52.4785217, lng: -1.8902777},
         zoom: 15,
         styles: style
     });
+
     var largeInfowindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
-    for (var i = 0; i < locationStore.length; i++) {
-        var position = locationStore[i].location;
-        var title = locationStore[i].title;
+    map.fitBounds(bounds);
+
+    for (var i = 0; i < restaurants.length; i++) {
+        var lat = restaurants[0].restaurant.location.latitude;
+        var lng = restaurants[0].restaurant.location.longitude;
+        var position = {'lat': lat, 'lng': lng};
+        var title = restaurants[i].restaurant.name;
         var marker = new google.maps.Marker({
             map: map,
             position: position,
@@ -194,12 +184,11 @@ function initMap(style) {
             id: i
         });
         markers.push(marker);
-        marker.addListener('click', function() {
+        marker.addListener('click', function () {
             populateInfoWindow(this, largeInfowindow);
         });
         bounds.extend(markers[i].position);
     }
-    map.fitBounds(bounds);
 }
 
 function populateInfoWindow(marker, infowindow) {
@@ -241,7 +230,7 @@ function ViewModel() {
     self.showlocations = ko.observableArray();
 
     self.init = function () {
-        for (i = 0; i < locationStore.length; i++)
+        /*for (i = 0; i < locationStore.length; i++)
         {
             self.showlocations.push(new locationItem
                 (
@@ -250,7 +239,7 @@ function ViewModel() {
                     locationStore[i].location
                 )
             );
-        }
+        }*/
     };
 }
 
