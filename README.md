23320a9cc52d94011d077e6242bed1170adbe1bf
Taysseer Kadri <taysseer.kadri@gmail.com>
2017-10-29 22:42:19 -0700

    First full submission

---
+++ README.md
@@ -0,0 +1,11 @@
+## Neighborhood Map Project by Taysseer Kadri
+
+### Introduction
+This project is my submission for the Neighborhood Map Project for the Udacity Full Stack Developer Program.
+
+In this project, you can explore a stylized map of Digbeth, Birmingham with a filterable list of restaurants in the area.
+
+### How to Use
+Simply load the index.html file in your favorite web browser and play with the map!
+
+For any questions, email taysseer.kadri@gmail.com
--- js/script.js
+++ js/script.js
@@ -149,7 +149,6 @@ var nightstyles = [
 ];
 var restaurants = {};
 
-
 //Call Zomato API and retrieve top 20 locations around the neighborhood
 $.ajax({
     url: zomatoCall,
@@ -159,6 +158,8 @@ $.ajax({
         restaurants = data['restaurants']
     }
 });
+
+console.log(restaurants);
 
 // Create a model structure for the Knockout ViewModel
 // to follow when creating items based on the API Call results.
@@ -184,22 +185,22 @@ function ViewModel() {
     // allLocations is a list of ALL locations, will be used as a default list for reference
     self.allLocations = ko.observableArray();
 
+    // night time
+    //self.isNight = ko.observable(false);
+    //self.allNightLocations = ko.observableArray();
+
     // On init, load the location values in allLocations and then set the showlocations to
     // be equivalent to it.
-    self.init = function () {
+    self.init = function ()
+    {
         for (var i = 0; i < restaurants.length; i++) {
-
             // THE API DOES NOT INCLUDE AFTER HOURS INFO.
             // WE INCLUDE IT HERE ARTIFICIALLY
             // FOR THE PURPOSES OF THIS PROJECT.
-
             var afterhours = false;
-            if(i % 2 == 0) {
-                afterhours = true;
-            }
+            if(i % 2 === 0) { afterhours = true; }
             self.allLocations.push(
                 new locationItem(
-
                     restaurants[i].restaurant.name,
                     restaurants[i].restaurant.cuisines,
                     restaurants[i].restaurant.location["address"],
@@ -208,22 +209,54 @@ function ViewModel() {
                     restaurants[i].restaurant.location.longitude
                 )
             );
+            /**if (afterhours === true){
+                self.allNightLocations.push(
+                    new locationItem(
+                        restaurants[i].restaurant.name,
+                        restaurants[i].restaurant.cuisines,
+                        restaurants[i].restaurant.location["address"],
+                        afterhours,
+                        restaurants[i].restaurant.location.latitude,
+                        restaurants[i].restaurant.location.longitude
+                    )
+                );
+            }**/
         }
         self.showlocations(self.allLocations());
     };
+
+
 
     // filterResults is a function that gets the value of the search query,
     // and builds a temporary array with the search query affecting the results.
     // The final results are then loaded into the showlocations array.
     // this function is called on every keyup event.
 
+    /**self.toggleNight = function () {
+        var isnight = self.isNight();
+        if(isnight === true)
+        {
+            self.isNight(false);
+            self.showlocations(self.allLocations);
+        }
+        else{
+            self.isNight(true);
+            self.showlocations(self.allNightLocations());
+        }
+        return true;
+    };**/
+
     self.filterResults = function(data, event){
         var tempLocations = ko.observableArray();
         var params = event.target.value;
-        self.allLocations().forEach(function(v,i){
+        self.allLocations().forEach(function(v,i)
+        {
             var name = v.name();
-            var search = name.toLowerCase().search(params.toLowerCase());
-            if (search > -1){
+            var cuisines = v.cuisines();
+            var searchName = name.toLowerCase().search(params.toLowerCase());
+            var searchCuisines = cuisines.toLowerCase().search(params.toLowerCase());
+            if (searchName > -1 || searchCuisines > -1)
+            {
                 tempLocations.push(v);
             }
         });
@@ -233,7 +266,13 @@ function ViewModel() {
     // This function focuses the map on the marker relative to the restaurant
     // chosen.
     self.selectLocation = function (param) {
-        console.log(param);
+        console.clear();
+        markers.forEach(function (v, i) {
+            if (v.title === param.name())
+            {
+                populateInfoWindow(v, new google.maps.InfoWindow());
+            }
+        });
     };
 }
 
@@ -241,11 +280,9 @@ function ViewModel() {
 // Set map style to day time style by default
 // Get coordinates of all locations in JSON data,
 // and create array of markers that need to be rendered.
-
 function initMap(style) {
-    if (style == null) {
-        style = daystyles
-    }
+    if (style == null){style = daystyles}
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 52.4771526, lng: -1.8907827},
         zoom: 15,
@@ -253,40 +290,92 @@ function initMap(style) {
     });
 
     var largeInfowindow = new google.maps.InfoWindow();
+
     var bounds = new google.maps.LatLngBounds(
         new google.maps.LatLng(52.4411273,-1.9444738),
         new google.maps.LatLng(52.4942329,-1.8864697)
-
     );
     map.fitBounds(bounds);
+    function makeMarkerIcon(markerColor)
+    {
+        var markerImage = new google.maps.MarkerImage(
+            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+            '|40|_|%E2%80%A2',
+            new google.maps.Size(21, 34),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(10, 34),
+            new google.maps.Size(21,34));
+        return markerImage;
+    }
+    // Style the markers a bit. This will be our listing marker icon.
+    // var defaultIcon = makeMarkerIcon('0091ff');
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    // var highlightedIcon = makeMarkerIcon('FFFF24');
+    var defaultIcon;
+    var highlightedIcon;
+    if (style === daystyles){
+        defaultIcon = makeMarkerIcon('660066');
+        highlightedIcon = makeMarkerIcon('ff6600');
+    }
+    else if (style === nightstyles){
+        defaultIcon = makeMarkerIcon('ffff1a');
+        highlightedIcon = makeMarkerIcon('99ff33');
+    }
 
     for (var i = 0; i < restaurants.length; i++) {
         var lat = restaurants[i].restaurant.location.latitude;
         var lng = restaurants[i].restaurant.location.longitude;
         var position = {lat: parseFloat(lat), lng: parseFloat(lng)};
         var title = restaurants[i].restaurant.name;
+        var id = restaurants[i].restaurant.id;
+        var cuisine = restaurants[i].restaurant.cuisines;
+        var menu_url = restaurants[i].restaurant.menu_url;
+        var thumb_url = restaurants[i].restaurant.thumb;
+
         var marker = new google.maps.Marker({
             map: map,
             position: position,
+            id: id,
             title: title,
+            cuisine: cuisine,
+            menu_url: menu_url,
+            thumb_url: thumb_url,
             animation: google.maps.Animation.DROP,
+            icon: defaultIcon,
             id: i
         });
         markers.push(marker);
         marker.addListener('click', function () {
             populateInfoWindow(this, largeInfowindow);
         });
+        marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+        });
+        marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+        });
         //bounds.extend(markers[i].position);
     }
 }
-
 
 // Check to make sure to populate the infoWindow.
 function populateInfoWindow(marker, infowindow) {
 
     if (infowindow.marker != marker) {
         infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.setContent
+        (
+            '<div class="container" style="max-width: 400px;">' +
+            '<div class="row">' +
+            '<div class="col-xs-6"><img width="128px;" src="' + marker.thumb_url + '"/></div>' +
+            '<div class="col-xs-6">' +
+            '<h3>' + marker.title + '</h3>' +
+            '<p>' + marker.cuisine + ' Restaurant </p>' +
+            '<p><a href=" '+ marker.menu_url +'">See Menu</a></p>' +
+            '</div></div></div>'
+        );
+
         infowindow.open(map, marker);
         // Make sure the marker property is cleared if the infowindow is closed.
         infowindow.addListener('closeclick', function () {
@@ -306,9 +395,6 @@ $("#afterhours").change(function () {
         initMap(daystyles);
     }
 });
-
-
-
 
 var viewModel = new ViewModel();
 ko.applyBindings(viewModel);
